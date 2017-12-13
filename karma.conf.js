afb8089c15088a72be7e621791d9096b9a404eeb
Yang <wenyiyang@lm-sfa-11003006.corp.ebay.com>
2017-12-12 18:08:37 -0800

    add karma and jasmin for unit test

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+var baseConfig = require('./webpack.config');
+var merge = require('webpack-merge');
+delete baseConfig.entry;
+
+var webpackConfig = merge(baseConfig, {
+    // vue: {
+    //     loaders: {
+    //         js: 'isparta'
+    //     }
+    // }
+    entry: './test/unit/index.js'
+});
+
+module.exports = function(config) {
+    config.set({
+        browsers: ['PhantomJS'],
+        frameworks: ['jasmine'],
+        reporters: ['progress', 'coverage', 'verbose'],
+        // this is the entry file for all our tests.
+        files: ['./test/unit/index.js'],
+        // we will pass the entry file to webpack for bundling.
+        preprocessors: {
+            './test/unit/index.js': ['webpack'],
+            './test/unit/specs/*.spec.js': ['babel']
+        },
+        coverageReporter: {
+            dir: './test/coverage',
+            reporters: [
+                {type: 'html'},
+                {type: 'text-summary'}
+            ]
+        },
+        webpack: webpackConfig,
+        webpackMiddleware: {
+            noInfo: true
+        },
+        singleRun: true
+    });
+};
---
+++ test/unit/specs/main.spec.js
@@ -0,0 +1,7 @@
+/* global describe, it, expect */
+
+describe('add', () => {
+    it('should have correct text', () => {
+        expect(1 + 2).toEqual(3);
+    });
+});
